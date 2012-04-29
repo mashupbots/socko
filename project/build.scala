e2dae3511cd1b359f828069b938a6eb3bfae33e1
vibul <vibuli@gmail.com>
2012-04-29 17:01:21 +1000

    set version to 0.1.0

--- project/build.scala
+++ project/build.scala
@@ -18,7 +18,7 @@ object SockoBuild extends Build {
   lazy val defaultSettings = Defaults.defaultSettings ++ Seq(
     // Info
     organization := "org.mashupbots.socko",
-    version      := "0.1.0-SNAPSHOT",
+    version      := "0.1.0",
 
     // Repositories
     resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
