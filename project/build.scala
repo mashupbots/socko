285d681d5a16fd6cdee10ffb53ca6e5eef5b67a0
vibul <vibuli@gmail.com>
2012-09-06 08:38:50 +1000

    Akka test dependency only needed for testing

--- project/build.scala
+++ project/build.scala
@@ -126,7 +126,7 @@ object Dependencies {
 object Dependency {
   val akkaActor     = "com.typesafe.akka"      % "akka-actor"         % "2.0.2"
   val akkaSlf4j     = "com.typesafe.akka"      % "akka-slf4j"         % "2.0"
-  val akkaTestKit   = "com.typesafe.akka"      % "akka-testkit"       % "2.0"
+  val akkaTestKit   = "com.typesafe.akka"      % "akka-testkit"       % "2.0"           % "test"
   val netty         = "io.netty"               % "netty"              % "3.5.5.Final"
   val logback       = "ch.qos.logback"         % "logback-classic"    % "1.0.3"         % "runtime"
   val scalatest     = "org.scalatest"          %% "scalatest"         % "2.0.M2"        % "test"
