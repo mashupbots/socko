726b29af83f7d69d0186bc4035042b390ec0ac88
vibul <vibuli@gmail.com>
2012-09-10 13:27:25 +1000

    Bump version 0.2.3

--- project/build.scala
+++ project/build.scala
@@ -20,7 +20,7 @@ object SockoBuild extends Build {
   lazy val defaultSettings = Defaults.defaultSettings ++ Seq(
     // Info
     organization := "org.mashupbots.socko",
-    version      := "0.2.2",
+    version      := "0.2.3",
 
     // Repositories
     resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
